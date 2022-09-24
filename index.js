320b5f5b8396dc2010fe389399aac26780ec582b
Alex Bondo Andersen <abondoa@gmail.com>
2022-09-25 00:18:09 +0200

    Moving

--- index.js
+++ index.js
@@ -1,100 +1,132 @@
-// Imports
-const Discord = require("discord.js");
-const Compute = require("@google-cloud/compute");
-const discordConfig = require("./discord-config.json");
+// Require the necessary discord.js classes
+const {
+  Client,
+  GatewayIntentBits,
+  REST,
+  SlashCommandBuilder,
+  Routes,
+} = require("discord.js");
+const { clientId, token } = require("./config.json");
+const compute = require("@google-cloud/compute");
 const gcpConfig = require("./GCP_config.json");
 
-// Initialize
-const client = new Discord.Client();
-const compute = new Compute({
-  projectId: gcpConfig.project_id,
-  keyFilename: "./GCP_config.json",
+// Create a new client instance
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageTyping],
 });
+const instancesClient = new compute.InstancesClient();
 
-// Global vars
-let serverInstance;
-let metadataServerInstance;
-let statusServerInstance;
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Replies with server info"),
+  new SlashCommandBuilder()
+    .setName("start")
+    .setDescription("Starts the server"),
+  new SlashCommandBuilder().setName("stop").setDescription("Stops the server"),
+].map((command) => command.toJSON());
 
-// Fetch VMs from GCP, async
-async function getListOfVMs() {
-  const options = {
-    maxResults: 3,
-  };
-  const [vms] = await compute.getVMs(options);
-  serverInstance = vms[0];
-  metadataServerInstance = serverInstance.metadata;
-  statusServerInstance = metadataServerInstance.status;
-}
+const rest = new REST({ version: "10" }).setToken(token);
+const instanceQuery = {
+  project: gcpConfig.project_id,
+  zone: "europe-west1-b",
+  instance: "instance-1",
+};
 
-// When the bot gets ready
-client.once("ready", async () => {
-  await getListOfVMs();
-  console.log("The bot is ready now");
-});
+rest
+  .put(Routes.applicationCommands(clientId), { body: commands })
+  .then((data) =>
+    console.log(`Successfully registered ${data.length} application commands.`)
+  )
+  .catch(console.error);
 
-// Listen for messages
-client.on("message", async (message) => {
-  const userMessage = message.content;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  console.log(
+    `Got interaction:\n${JSON.stringify(
+      interaction,
+      (key, value) => (typeof value === "bigint" ? value.toString() : value),
+      4
+    )}`
+  );
 
-  if (userMessage.startsWith("!server")) {
-    const code =
-      userMessage.split(" ")[1] == null ? "" : userMessage.split(" ")[1].trim();
+  const { commandName } = interaction;
 
-    if (code.length == 0 || code == null || code == "") {
-      message.channel.send(
-        "Umm, you were supposed to type `!server start` or `!server stop`"
-      );
-      message.channel.send(
-        "You can also type `!server status` to get the current status of the server"
-      );
-    }
-
-    if (code.toLowerCase() == "status") {
-      const [vms] = await compute.getVMs({ maxResults: 3 });
-      const status = vms[0].metadata.status;
-
-      message.channel.send("serverInstance is currently: **" + status + "**");
-    }
-
-    if (code.toLowerCase() == "start") {
-      if (statusServerInstance == "RUNNING") {
-        const [vms] = await compute.getVMs({ maxResults: 3 });
-        const paperInstance = vms[0];
-        const sshCommand =
-          'gcloud beta compute ssh --zone "YOUR_ZONE" "serverInstance" --project "YOUR_GCP_PROJECT_NAME"';
-
-        const ip =
-          paperInstance.metadata.networkInterfaces[0].accessConfigs[0].natIP;
-        message.channel.send("serverInstance running on IP: **" + ip + "**");
-        message.channel.send("SSH Command: `" + sshCommand + "`");
-      }
-      if (statusServerInstance == "TERMINATED") {
-        serverInstance.start().then(async (data) => {
-          message.channel.send(
-            "Provisioning the serverInstance. Run **`!server start`** after a few seconds to get the IP Address."
-          );
-          message.channel.send(
-            "You can also type `!server status` to get the current status of the server"
-          );
-          statusServerInstance = "RUNNING";
-        });
-      }
-    }
-
-    if (code.toLowerCase() == "stop") {
-      if (statusServerInstance == "RUNNING") {
-        serverInstance.stop().then((data) => {
-          message.channel.send("Server stopped");
-        });
-      }
-      if (statusServerInstance == "TERMINATED") {
-        message.channel.send("Server was already stopped");
-      }
-      statusServerInstance = "TERMINATED";
-    }
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await server(interaction.reply.bind(interaction));
+  } else if (commandName === "start") {
+    await start(interaction);
+  } else if (commandName === "stop") {
+    await stop(interaction);
   }
 });
 
-// Login
-client.login(discordConfig.token);
+async function server(reply) {
+  const [instance] = await instancesClient.get(instanceQuery);
+
+  console.log(
+    `Instance ${instanceQuery.instance} data:\n${JSON.stringify(
+      instance,
+      null,
+      4
+    )}`
+  );
+  reply(
+    `Server is currently: ${instance.status}, on IP ${instance?.networkInterfaces[0]?.accessConfigs[0]?.natIP}`
+  );
+}
+
+async function start(interaction) {
+  const instancesClient = new compute.InstancesClient();
+
+  const [response] = await instancesClient.start(instanceQuery);
+  let operation = response.latestResponse;
+  const operationsClient = new compute.ZoneOperationsClient();
+  await interaction.deferReply();
+
+  // Wait for the operation to complete.
+  while (operation.status !== "DONE") {
+    [operation] = await operationsClient.wait({
+      operation: operation.name,
+      project: instanceQuery.project,
+      zone: operation.zone.split("/").pop(),
+    });
+  }
+
+  console.log("Instance started.");
+  await server(interaction.editReply.bind(interaction));
+}
+
+async function stop(interaction) {
+  const instancesClient = new compute.InstancesClient();
+
+  const [response] = await instancesClient.stop(instanceQuery);
+  let operation = response.latestResponse;
+  const operationsClient = new compute.ZoneOperationsClient();
+  await interaction.deferReply();
+
+  // Wait for the operation to complete.
+  while (operation.status !== "DONE") {
+    [operation] = await operationsClient.wait({
+      operation: operation.name,
+      project: instanceQuery.project,
+      zone: operation.zone.split("/").pop(),
+    });
+  }
+
+  console.log("Instance stopped.");
+  await server(interaction.editReply.bind(interaction));
+}
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// Login to Discord with your client's token
+client.login(token);
